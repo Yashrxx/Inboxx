30bbd312f8e44e232963cb37f3717f72fdd4e23c
Yash Kumar S <yashkumar.s2023@vitstudent.ac.in>
2026-08-19 18:18:00 +0530

    trying to fix images issue on vercel - 2

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -2,12 +2,16 @@
  * Universal image asset paths.
  * Points directly to static assets in the root public/ directory.
  */
-export const HOME_HERO_IMG = "/home_hero.jpg";
-export const LOGIN_HERO_IMG = "/login_hero.jpg";
-export const SIGNUP_HERO_IMG = "/signup_hero.jpg";
+import homeHero from "../assets/images/workspace_hero_bg_1787032051238.jpg";
+import loginHero from "../assets/images/office_hero_login_1787028506237.jpg";
+import signupHero from "../assets/images/office_team_signup_1787029114301.jpg";
+
+export const HOME_HERO_IMG = homeHero;
+export const LOGIN_HERO_IMG = loginHero;
+export const SIGNUP_HERO_IMG = signupHero;
 
 export const IMAGE_FALLBACKS = {
-  home: ["/home_hero.jpg", "/images/home_hero.jpg"],
-  login: ["/login_hero.jpg", "/images/login_hero.jpg"],
-  signup: ["/signup_hero.jpg", "/images/signup_hero.jpg"],
+  home: [homeHero, "/home_hero.jpg", "/images/home_hero.jpg"],
+  login: [loginHero, "/login_hero.jpg", "/images/login_hero.jpg"],
+  signup: [signupHero, "/signup_hero.jpg", "/images/signup_hero.jpg"],
 };
